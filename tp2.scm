4971ad5eb1b8f71188d54c4ef069228e64d55164
Vincent Foley <vfoley@gmail.com>
2010-11-11 13:14:14 -0500

    Added translation and rotation

--- tp2.scm
+++ tp2.scm
@@ -4,16 +4,33 @@
 ;; Vincent Foley-Bourgon (FOLV08078309)
 
 
+
+
 (define *distance-minimale* 1/10)
-
-;; identity function
-;; T -> T
-(define id
-  (lambda (x)
-    x))
+(define *pi* 3.141592)
 
 
 
+;;;;; Fonctions mathématiques
+(define translate-segm
+  (lambda (s dx dy)
+    (let ((translate-x (lambda (x) (+ x dx)))
+          (translate-y (lambda (y) (+ y dy))))
+      (segm (modifier-vect (segm-depart s) translate-x translate-y)
+            (modifier-vect (segm-arrivee s) translate-x translate-y)))))
+
+
+(define rotate-vect
+  (lambda (v angle)
+    (vect (+ (* (vect-x v) (cos angle))
+             (* (vect-y v) (sin angle)))
+          (- (* (vect-y v) (cos angle))
+             (* (vect-x v) (sin angle))))))
+
+(define rotate-segm
+  (lambda (s angle)
+    (segm (rotate-vect (segm-depart s) angle)
+          (rotate-vect (segm-arrivee s) angle))))
 
 (define point-milieu
   (lambda (v1 v2)
@@ -26,7 +43,6 @@
           (dy (- (vect-y v1) (vect-y v2))))
       (sqrt (+ (* dx dx) (* dy dy))))))
 
-
 (define liste-points
   (lambda (t a d acc)
     (if (< (distance a d) *distance-minimale*)
@@ -34,14 +50,32 @@
         (let ((m (point-milieu d a)))
           (liste-points t a m
                         (liste-points t m d acc))))))
+;;;;;
 
-;; (define liste-points
-;;   (lambda (t d a)
-;;     (if (< (distance d a) *distance-minimale*)
-;;         (list (segm (t d) (t a)))
-;;         (let ((m (point-milieu d a)))
-;;           (append (liste-points t a m)
-;;                   (liste-points t m d))))))
+
+
+
+;;;;; Fonctions utilitaires
+(define id
+  (lambda (x)
+    x))
+
+
+(define o
+  (lambda (f1 f2)
+    (lambda (x)
+      (f1 (f2 x)))))
+
+;; Retourne (vect (fx v.x) (fy v.y))
+(define modifier-vect
+  (lambda (v fx fy)
+    (vect (fx (vect-x v))
+          (fy (vect-y v)))))
+;;;;;
+
+
+
+
 
 (define ligne
   (lambda (depart arrivee)
@@ -60,6 +94,18 @@
                     (else (append (liste-points transf (car lst) (cadr lst) '())
                                   (loop (cdr lst))))))))
         (loop vect-list)))))
+
+
+(define translation
+  (lambda (dx dy dessinateur)
+    (lambda (transf)
+      (map (lambda (segment) (translate-segm segment dx dy)) (dessinateur transf)))))
+
+
+(define rotation
+  (lambda (angle dessinateur)
+    (lambda (transf)
+      (map (lambda (segment) (rotate-segm segment angle)) (dessinateur transf)))))
 
 
 (define ell
