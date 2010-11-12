531d2e969ab669ea48f27cf2e9e4d3973cbacdf8
Vincent Foley <vfoley@gmail.com>
2010-11-11 21:31:31 -0500

    Added loupe

--- tp2.scm
+++ tp2.scm
@@ -12,6 +12,29 @@
 
 
 ;;;;; Fonctions mathématiques
+(define degre->radian
+  (lambda (x)
+    (* *pi* (/ x 180))))
+
+(define point-milieu
+  (lambda (v1 v2)
+    (vect (/ (+ (vect-x v1) (vect-x v2)) 2)
+          (/ (+ (vect-y v1) (vect-y v2)) 2))))
+
+(define distance
+  (lambda (v1 v2)
+    (let ((dx (- (vect-x v1) (vect-x v2)))
+          (dy (- (vect-y v1) (vect-y v2))))
+      (sqrt (+ (* dx dx) (* dy dy))))))
+
+(define liste-points
+  (lambda (t a d acc)
+    (if (< (distance a d) *distance-minimale*)
+        (cons (segm (t a) (t d)) acc)
+        (let ((m (point-milieu d a)))
+          (liste-points t a m
+                        (liste-points t m d acc))))))
+
 (define translate-segm
   (lambda (s dx dy)
     (let ((translate-x (lambda (x) (+ x dx)))
@@ -39,24 +62,20 @@
     (segm (rotate-vect (segm-depart s) angle)
           (rotate-vect (segm-arrivee s) angle))))
 
-(define point-milieu
-  (lambda (v1 v2)
-    (vect (/ (+ (vect-x v1) (vect-x v2)) 2)
-          (/ (+ (vect-y v1) (vect-y v2)) 2))))
+(define loupe-vect
+  (lambda (v fact)
+    (let* ((x (vect-x v))
+           (y (vect-y v))
+           (m (/ (+ 1 fact)
+                 (+ 1 (* fact
+                         (+ (* x x)
+                            (* y y)))))))
+      (vect (* x m) (* y m)))))
 
-(define distance
-  (lambda (v1 v2)
-    (let ((dx (- (vect-x v1) (vect-x v2)))
-          (dy (- (vect-y v1) (vect-y v2))))
-      (sqrt (+ (* dx dx) (* dy dy))))))
-
-(define liste-points
-  (lambda (t a d acc)
-    (if (< (distance a d) *distance-minimale*)
-        (cons (segm (t a) (t d)) acc)
-        (let ((m (point-milieu d a)))
-          (liste-points t a m
-                        (liste-points t m d acc))))))
+(define loupe-segm
+  (lambda (s fact)
+    (segm (loupe-vect (segm-depart s) fact)
+          (loupe-vect (segm-arrivee s) fact))))
 ;;;;;
 
 
@@ -112,12 +131,20 @@
 (define rotation
   (lambda (angle dessinateur)
     (lambda (transf)
-      (map (lambda (segment) (rotate-segm segment angle)) (dessinateur transf)))))
+      (map (lambda (segment)
+             (rotate-segm segment (degre->radian angle)))
+           (dessinateur transf)))))
 
 (define reduction
   (lambda (factx facty dessinateur)
     (lambda (transf)
       (map (lambda (segment) (etire-segm segment factx facty))
+           (dessinateur transf)))))
+
+(define loupe
+  (lambda (fact dessinateur)
+    (lambda (transf)
+      (map (lambda (segment) (loupe-segm segment fact))
            (dessinateur transf)))))
 
 
